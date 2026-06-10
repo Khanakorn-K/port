02f8f1b31dfd58301e053af8099b047ee1297ae7
khanakorn kositkhongchana <khanakorn01@gmail.com>
2026-06-10 19:27:58 +0700

    fix: update Next.js security patch

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
 import type { NextConfig } from "next";
 import dotenv from "dotenv";
 import path from "path";
-//
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
 dotenv.config({ path: path.resolve(__dirname, "./env/.env.local") });
-//
+
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "standalone",
   images: {
     domains: ["assets.aceternity.com", "images.unsplash.com"],
@@ -15,9 +17,6 @@ const nextConfig: NextConfig = {
     NEXT_PUBLIC_SERVICE_ID: process.env.NEXT_PUBLIC_SERVICE_ID,
     NEXT_PUBLIC_TEMPLATE_ID: process.env.NEXT_PUBLIC_TEMPLATE_ID,
     NEXT_PUBLIC_PUBLIC_KEY: process.env.NEXT_PUBLIC_PUBLIC_KEY,
-  },
-  eslint: {
-    ignoreDuringBuilds: true, // ✅ ข้าม ESLint เมื่อ build
   },
   experimental: {},
 };
