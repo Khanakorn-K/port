069da7188eaf7495d7c2396ede7c764ead6dac5c
khanakorn kositkhongchana <khanakorn01@gmail.com>
2026-06-10 19:29:56 +0700

    fix: update next config for vercel build

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,10 @@
 import type { NextConfig } from "next";
-import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-dotenv.config({ path: path.resolve(__dirname, "./env/.env.local") });
 
 const nextConfig: NextConfig = {
   output: "standalone",
   images: {
     domains: ["assets.aceternity.com", "images.unsplash.com"],
   },
-  env: {
-    NEXT_PUBLIC_SERVICE_ID: process.env.NEXT_PUBLIC_SERVICE_ID,
-    NEXT_PUBLIC_TEMPLATE_ID: process.env.NEXT_PUBLIC_TEMPLATE_ID,
-    NEXT_PUBLIC_PUBLIC_KEY: process.env.NEXT_PUBLIC_PUBLIC_KEY,
-  },
-  experimental: {},
 };
 
 export default nextConfig;
